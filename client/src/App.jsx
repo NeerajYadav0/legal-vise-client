0e89939f05f14c96f04c10e6f5d32b3b31b6a3af
raghavjindalll <raghavjindal1253@gmail.com>
2024-02-16 12:24:16 +0530

    Cleaning up repo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,17 @@
 import "./App.css";
 import { Button } from "./components/ui/button";
+import { toast } from "sonner";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { useState } from "react";
 
 function App() {
+  function clickHandler() {
+    toast.success("Event has been created.");
+  }
+
   return (
     <>
-      <Button>Hello</Button>
+      <Button onClick={clickHandler}>Hello</Button>
     </>
   );
 }
