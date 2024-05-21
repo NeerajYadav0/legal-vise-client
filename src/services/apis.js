6d7d3a8a31225933fd73086389cd39832685ff44
NeerajYadav0 <neerajrao04@gmail.com>
2024-05-21 23:13:10 +0530

    changes

--- src/services/apis.js
+++ src/services/apis.js
@@ -1,5 +1,5 @@
-const BASE_URL = "http://localhost:8000/api/";
-// const BASE_URL = "https://legal-vise-server.vercel.app/";
+// const BASE_URL = "http://localhost:8000/api/";
+const BASE_URL = "https://legal-vise-server.vercel.app/";
 
 // AUTH ENDPOINTS
 export const endpoints = {
