0247dc54283fe8ba8aaec8e32a87d7c13d4cab3a
NeerajYadav0 <neerajrao04@gmail.com>
2024-05-28 21:41:29 +0530

    cors

--- src/services/apiConnector.js
+++ src/services/apiConnector.js
@@ -11,8 +11,6 @@ export const apiConnector = (method, url, bodyData, headers, params) => {
       ? headers
       : {
           Authorization: `Bearer ${token}`,
-          "Access-Control-Allow-Origin":
-            "https://legal-vise-client-nmhxqnfpk-neerajyadav0s-projects.vercel.app",
         },
     params: params ? params : null,
   });
